bdf1530f9a82c416a3799c0ef5a63970f56641c5
Scott <scotts@nsidc.org>
2023-04-25 16:20:37 +0000

    use pytest fixtures in integration tests

--- seaice_ecdr/tests/integration/test_initial_daily_ecdr_generation.py
+++ seaice_ecdr/tests/integration/test_initial_daily_ecdr_generation.py
@@ -7,7 +7,10 @@ amsr2_cdr results
 
 import datetime as dt
 from pathlib import Path
+import pytest
+from loguru import logger
 
+import sys
 import numpy as np
 import xarray as xr
 from numpy.testing import assert_almost_equal, assert_equal
@@ -17,13 +20,15 @@ from pm_icecon.cdr import amsr2_cdr as pmi_amsr2_cdr
 from seaice_ecdr.initial_daily_ecdr import compute_initial_daily_ecdr_dataset as compute_idecdr_ds
 
 
-def test_testing_initial_daily_ecdr_generation():
-    """ Test that this integration test is being performed """
-    assert True
+# Set the default minimum log notification to Warning
+logger.remove(0)  # Removes previous logger info
+logger.add(sys.stderr, level="WARNING")
 
 
-def test_pmicecon_conc_generation():
-    """ Test that pm_icecon yields a 'conc' field """
+@pytest.fixture(scope='session')
+def sample_pmicecon_dataset():
+    logger.info(f'testing: Creating sample pmicecon dataset')
+
     test_date = dt.datetime(2021, 4, 5).date()
     test_hemisphere = 'north'
     test_resolution = '12'
@@ -33,30 +38,50 @@ def test_pmicecon_conc_generation():
         hemisphere=test_hemisphere,
         resolution=test_resolution,
     )
-    assert type(pmicecon_conc_ds) == type(xr.Dataset())
-
-    pmicecon_conc_varname = 'conc'
-    assert type(pmicecon_conc_ds.variables[pmicecon_conc_varname]) == xr.core.variable.Variable
+    return pmicecon_conc_ds
 
 
-def test_seaice_idecdr_and_pmicecon_conc_identical():
-    """ Test that pm_icecon yields a 'conc' field """
+@pytest.fixture(scope='session')
+def sample_idecdr_dataset():
+    logger.info(f'testing: Creating sample idecdr dataset')
+
     test_date = dt.datetime(2021, 4, 5).date()
     test_hemisphere = 'north'
     test_resolution = '12'
-
-    pmicecon_conc_ds = pmi_amsr2_cdr(
-        date=test_date,
-        hemisphere=test_hemisphere,
-        resolution=test_resolution,
-    )
-    pmi_conc_field = np.array(pmicecon_conc_ds.variables['conc'])
 
     ide_conc_ds = compute_idecdr_ds(
         date=test_date,
         hemisphere=test_hemisphere,
         resolution=test_resolution,
     )
+    return ide_conc_ds
+
+
+def test_testing_initial_daily_ecdr_generation():
+    """ Test that this integration test is being performed """
+    assert True
+
+
+def test_pmicecon_fixture(sample_pmicecon_dataset):
+    """ Test that pm_icecon yields a 'conc' field """
+    assert type(sample_pmicecon_dataset) == type(xr.Dataset())
+
+
+def test_pmicecon_conc_generation(sample_pmicecon_dataset):
+    """ Test that pm_icecon yields a 'conc' field """
+    #pmicecon_conc_ds = sample_pmicecon_dataset
+
+    pmicecon_conc_varname = 'conc'
+    assert type(sample_pmicecon_dataset.variables[pmicecon_conc_varname]) == xr.core.variable.Variable
+
+
+def test_seaice_idecdr_and_pmicecon_conc_identical(sample_pmicecon_dataset, sample_idecdr_dataset):
+    """ Test that pm_icecon yields a 'conc' field """
+
+    pmicecon_conc_ds = sample_pmicecon_dataset
+    pmi_conc_field = np.array(pmicecon_conc_ds.variables['conc'])
+
+    ide_conc_ds = sample_idecdr_dataset
     ide_conc_field = np.array(ide_conc_ds.variables['conc'])
 
     assert_equal(pmi_conc_field, ide_conc_field)
