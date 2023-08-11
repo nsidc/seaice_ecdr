92a862a154fd4f4500edb3d9480f692c1fc7af71
Scott <scotts@nsidc.org>
2023-08-10 23:44:29 -0600

    update dev pytest script

--- run_ecdr_pytest.sh
+++ run_ecdr_pytest.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Unit tests
 pytest -v -s -x seaice_ecdr/tests/unit/test_initial_daily_ecdr.py
+
+# Integration tests
 pytest -v -s -x seaice_ecdr/tests/integration/test_initial_daily_ecdr_generation.py
