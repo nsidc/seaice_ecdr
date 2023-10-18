f694ea836fa5abdf263657f7ec30e00205425e28
Trey Stafford <trey.stafford@colorado.edu>
2023-10-18 13:37:01 -0600

    update `sciprts/cli.sh` for `seaice_ecdr`

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -2,9 +2,4 @@
 
 ARGS=$@
 
-THIS_DIR="$( cd "$(dirname "$0")"; pwd -P )"
-PM_ICECON_DIR=/home/vagrant/pm_icecon
-
-# PYTHONPATH=$THIS_DIR/.. python -m pm_icecon.cli.entrypoint $ARGS
-# PYTHONPATH=$THIS_DIR/..:${PM_ICECON_DIR} python -m seaice_ecdr.cli.entrypoint $ARGS
-PYTHONPATH=$THIS_DIR/..:${PM_ICECON_DIR} python -m pm_icecon.cli.entrypoint $ARGS
+python -m seaice_ecdr.cli.entrypoint $ARGS
