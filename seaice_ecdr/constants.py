62347899ca521be4c96f9f3737a47470b94f8f5e
Trey Stafford <trey.stafford@colorado.edu>
2023-10-19 17:18:48 -0600

    clarify intial daily output dir path (`final` -> `standard`)

--- seaice_ecdr/constants.py
+++ seaice_ecdr/constants.py
@@ -7,9 +7,11 @@ NSIDC_NFS_SHARE_DIR = Path("/share/apps/amsr2-cdr")
 
 # Outputs from the `seaice_ecdr` go to these locations.
 BASE_OUTPUT_DIR = NSIDC_NFS_SHARE_DIR / "ecdr_outputs"
-# Daily initial output from 'final' input data (e.g., AU_SI12)
-INITIAL_DAILY_OUTPUT_DIR = BASE_OUTPUT_DIR / "final" / "initial_daily"
-# Daily initial output from NRT LANCE AMSR2 data (AU_SI12_NRT_R04)
+# Daily initial/intermiedate output for 'standard' (not NRT) ECDR processing
+# (e.g, using input data from AU_SI12)
+INITIAL_DAILY_OUTPUT_DIR = BASE_OUTPUT_DIR / "standard" / "initial_daily"
+# Daily initial/intermiedate output for 'Near Real Time' (NRT) ECDR processing
+# (using data from AU_SI12_NRT_R04)
 NRT_INITIAL_DAILY_OUTPUT_DIR = BASE_OUTPUT_DIR / "nrt" / "initial_daily"
 
 # Location of LANCE AMSR2 NRT data files:
