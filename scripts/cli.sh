0778f9ae6d3bac23a42eaaf1ba47c4e6190f6498
Scott <scotts@nsidc.org>
2023-10-19 14:49:21 -0600

    add pm_tb_data to cli.sh PYTHONPATH

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -4,7 +4,8 @@ ARGS=$@
 
 THIS_DIR="$( cd "$(dirname "$0")"; pwd -P )"
 PM_ICECON_DIR=/home/vagrant/pm_icecon
+PM_TB_DATA_DIR=/home/vagrant/pm_tb_data
 
 # PYTHONPATH=$THIS_DIR/.. python -m pm_icecon.cli.entrypoint $ARGS
 # PYTHONPATH=$THIS_DIR/..:${PM_ICECON_DIR} python -m seaice_ecdr.cli.entrypoint $ARGS
-PYTHONPATH=$THIS_DIR/..:${PM_ICECON_DIR} python -m pm_icecon.cli.entrypoint $ARGS
+PYTHONPATH=$THIS_DIR/..:${PM_ICECON_DIR}:${PM_TB_DATA_DIR} python -m pm_icecon.cli.entrypoint $ARGS
