28c1b2393a420d947bf75b1a70cb0d423bb40087
Trey Stafford <trey.stafford@colorado.edu>
2023-10-20 15:08:56 -0600

    Remove missing `format` module
    Taken care of by `pre-commit`.

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,9 +1,7 @@
 """Add format and test to collections."""
 from invoke import Collection
 
-from . import format as _format
 from . import test
 
 ns = Collection()
-ns.add_collection(_format)  # type: ignore
 ns.add_collection(test)  # type: ignore
