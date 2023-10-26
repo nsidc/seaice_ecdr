25d8d3b110cd29bca93c482fab1d6115887faed2
Trey Stafford <trey.stafford@colorado.edu>
2023-10-26 14:11:39 -0600

    Access seaice hemi from dict

--- seaice_ecdr/compare/ref_data.py
+++ seaice_ecdr/compare/ref_data.py
@@ -13,7 +13,7 @@ import xarray as xr
 from pm_icecon.constants import DEFAULT_FLAG_VALUES
 from pm_icecon.util import date_range, get_ps12_grid_shape, get_ps25_grid_shape
 from pm_tb_data.fetch import au_si
-from pm_tb_data._types import Hemisphere
+from pm_tb_data._types import Hemisphere, NORTH, SOUTH
 from pyresample import AreaDefinition
 from pyresample.image import ImageContainerNearest
 from seaice.data.api import concentration_daily
@@ -67,8 +67,13 @@ def get_sea_ice_index(
 
     Concentrations are floating point values 0-100
     """
+    # The `seaice` library has a different defintiion for hemispheres.
+    seaice_hemisphere = {
+        NORTH: nt.NORTH,
+        SOUTH: nt.SOUTH,
+    }[hemisphere]
     gridset = concentration_daily(
-        hemisphere=nt.NORTH if hemisphere == nt.NORTH else nt.SOUTH,
+        hemisphere=seaice_hemisphere,
         year=date.year,
         month=date.month,
         day=date.day,
