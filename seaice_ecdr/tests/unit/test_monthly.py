d1a09e01bd11dcb00382067a43d2b4d5f6eafdeb
Trey Stafford <trey.stafford@colorado.edu>
2023-11-01 16:41:20 -0600

    Unit test check for min days in month

--- seaice_ecdr/tests/unit/test_monthly.py
+++ seaice_ecdr/tests/unit/test_monthly.py
@@ -1,7 +1,14 @@
+import datetime as dt
 from pathlib import Path
 
+import pytest
+import xarray as xr
+
 from seaice_ecdr.complete_daily_ecdr import get_ecdr_dir
-from seaice_ecdr.monthly import _get_daily_complete_filepaths_for_month
+from seaice_ecdr.monthly import (
+    _get_daily_complete_filepaths_for_month,
+    check_min_days_for_valid_month,
+)
 
 
 def test__get_daily_complete_filepaths_for_month(fs):
@@ -35,3 +42,36 @@ def test__get_daily_complete_filepaths_for_month(fs):
     )
 
     assert sorted(_fake_files_for_test_year_month) == sorted(actual)
+
+
+def test_check_min_day_for_valid_month():
+    def _mock_daily_ds_for_month(num_days: int) -> xr.Dataset:
+        return xr.Dataset(
+            data_vars=dict(time=[dt.date(2022, 3, x) for x in range(1, num_days + 1)])
+        )
+
+    # Check that no error is raised for AMSR2, full month's worth of data
+    check_min_days_for_valid_month(
+        daily_ds_for_month=_mock_daily_ds_for_month(31),
+        sat="am2",
+    )
+
+    # Check that an error is raised for AMSR2, not a full month's worth of data
+    with pytest.raises(RuntimeError):
+        check_min_days_for_valid_month(
+            daily_ds_for_month=_mock_daily_ds_for_month(19),
+            sat="am2",
+        )
+
+    # Check that an error is not raised for n07, with modified min worth of data
+    check_min_days_for_valid_month(
+        daily_ds_for_month=_mock_daily_ds_for_month(10),
+        sat="n07",
+    )
+
+    # Check that an error is raised for n07, not a full month's worth of data
+    with pytest.raises(RuntimeError):
+        check_min_days_for_valid_month(
+            daily_ds_for_month=_mock_daily_ds_for_month(9),
+            sat="n07",
+        )
