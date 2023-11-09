e67daca0e19a5d119abb406f3de6d2df6e630481
Scott <scotts@nsidc.org>
2023-11-09 14:24:22 -0700

    clean up varstack abstraction

--- seaice_ecdr/temporal_composite_daily.py
+++ seaice_ecdr/temporal_composite_daily.py
@@ -485,24 +485,7 @@ def temporally_interpolated_ecdr_dataset_for_au_si_tbs(
 
     # Update the cdr_conc var with temporally interpolated cdr_conc field
     #   by creating a DataArray with conc fields +/- interp_range around date
-    # ------------- ABSTRACT THIS -----------------
-    interp_varname = "conc"
-    var_stack = ide_ds.data_vars[interp_varname].copy()
-    for interp_date in iter_dates_near_date(target_date=date, day_range=interp_range):
-        if interp_date != date:
-            interp_ds = read_or_create_and_read_idecdr_ds(
-                date=interp_date,
-                hemisphere=hemisphere,
-                resolution=resolution,
-                ecdr_data_dir=ecdr_data_dir,
-            )
-            this_var = interp_ds.data_vars[interp_varname].copy()
-            var_stack = xr.concat([var_stack, this_var], "time")
-
-    var_stack = var_stack.sortby("time")
-
-    # ------------- WITH THIS -----------------
-    new_var_stack = create_sorted_var_timestack(
+    var_stack = create_sorted_var_timestack(
         varname="conc",
         date_list=[
             iter_date
@@ -517,28 +500,6 @@ def temporally_interpolated_ecdr_dataset_for_au_si_tbs(
             "ecdr_data_dir": ecdr_data_dir,
         },
     )
-    """
-    interp_varname = "conc"
-    new_var_stack = ide_ds.data_vars[interp_varname].copy()
-    for interp_date in iter_dates_near_date(target_date=date, day_range=interp_range):
-        if interp_date != date:
-            interp_ds = read_or_create_and_read_idecdr_ds(
-                date=interp_date,
-                hemisphere=hemisphere,
-                resolution=resolution,
-                ecdr_data_dir=ecdr_data_dir,
-            )
-            this_var = interp_ds.data_vars[interp_varname].copy()
-            new_var_stack = xr.concat([new_var_stack, this_var], "time")
-
-    new_var_stack = new_var_stack.sortby("time")
-    """
-    # ------------- CHECK -----------------
-    breakpoint()
-    print("about to assert var stacks are equal ...")
-    assert np.array_equal(var_stack, new_var_stack, equal_nan=True)
-
-    print("check that...")
 
     ti_var, ti_flags = temporally_composite_dataarray(
         target_date=date,
