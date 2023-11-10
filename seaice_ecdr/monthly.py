7efc897d471144ac6d69ce30b0bef39371647ee2
Trey Stafford <trey.stafford@colorado.edu>
2023-11-10 11:14:03 -0700

    `flag_values` -> `flag_masks`

--- seaice_ecdr/monthly.py
+++ seaice_ecdr/monthly.py
@@ -283,7 +283,7 @@ def calc_qa_of_cdr_seaice_conc_monthly(
         long_name="Passive Microwave Monthly Northern Hemisphere Sea Ice Concentration QC flags",
         standard_name="sea_ice_area_fraction status_flag",
         flag_meanings=" ".join(k for k in QA_OF_CDR_SEAICE_CONC_MONTHLY_FLAGS.keys()),
-        flag_values=" ".join(
+        flag_masks=" ".join(
             str(int(v)) for v in QA_OF_CDR_SEAICE_CONC_MONTHLY_FLAGS.values()
         ),
         grid_mapping="crs",
