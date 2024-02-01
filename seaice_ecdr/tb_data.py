1f676c9036701b3660b31c9418a3a926423b4276
Trey Stafford <trey.stafford@colorado.edu>
2024-02-01 12:31:36 -0700

    Cleanup unreachable statement

--- seaice_ecdr/tb_data.py
+++ seaice_ecdr/tb_data.py
@@ -142,13 +142,6 @@ def create_null_au_si_tbs(
         elif resolution == "25":
             xdim = 316
             ydim = 332
-    else:
-        value_error_string = f"""
-        Could not create null_set of TBs for
-          hemisphere: {hemisphere}
-          resolution: {resolution}
-        """
-        raise ValueError(value_error_string)
 
     null_array = np.zeros((ydim, xdim), dtype=np.float64)
     null_array[:] = np.nan
