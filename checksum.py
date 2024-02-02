abb4bf2a2f5eb221bddd6219e3b7f83e2a136a58
Trey Stafford <trey.stafford@colorado.edu>
2024-02-02 16:33:58 -0700

    Add checksum code (stolen from `seaice_cdr`)

---
+++ checksum.py
@@ -0,0 +1,25 @@
+import hashlib
+from pathlib import Path
+
+
+def _get_checksum(filepath):
+    with filepath.open("rb") as file:
+        return hashlib.md5(file.read()).hexdigest()
+
+
+def write_checksum_file(input_filepath, output_dir):
+    input_filepath = Path(input_filepath)
+
+    checksum = _get_checksum(input_filepath)
+
+    size_in_bytes = input_filepath.stat().st_size
+    filename = input_filepath.name
+
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    output_filepath = output_dir / (filename + ".mnf")
+
+    with open(output_filepath, "w") as checksum_file:
+        checksum_file.write(f"{filename},{checksum},{size_in_bytes}")
+
+    print(f"Wrote checksum file {output_filepath}")
