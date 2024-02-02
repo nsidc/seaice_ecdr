5b124de56a3f426d178e359cb40e7619598adefd
Trey Stafford <trey.stafford@colorado.edu>
2024-02-02 16:24:17 -0700

    Clone `pm_icecon` and `pm_tb_data` and archive those as well.

--- scripts/make_noaa_archive.py
+++ scripts/make_noaa_archive.py
@@ -18,14 +18,20 @@ from loguru import logger
 from seaice_ecdr.constants import CDR_ANCILLARY_DIR
 
 
-def clone_repo(*, clone_dir: Path, ref: str):
+def clone_repo(
+    *,
+    clone_address: str,
+    clone_dir: Path,
+    ref: str,
+    project_name="seaice_ecdr",
+):
     result = subprocess.run(
         [
             # Clone the repo from the source. This avoids copying any
             # non-tracked files from the user's local copy.
             "git",
             "clone",
-            "git@github.com:nsidc/seaice_ecdr.git",
+            clone_address,
         ],
         cwd=clone_dir,
     )
@@ -38,18 +44,37 @@ def clone_repo(*, clone_dir: Path, ref: str):
             "checkout",
             ref,
         ],
-        cwd=clone_dir / "seaice_ecdr",
+        cwd=clone_dir / project_name,
     )
 
     result.check_returncode()
 
 
-def make_archive_for_noaa(*, output_dir: Path, ref: str = "main"):
+def make_archive_for_noaa(
+    *, output_dir: Path, seaice_ecdr_ref: str, pm_icecon_ref: str, pm_tb_data_ref
+):
     with tempfile.TemporaryDirectory() as tempdir:
         archive_name = f"archive_{dt.date.today():%Y%m%d}"
         archive_dir = Path(tempdir) / archive_name
         archive_dir.mkdir()
-        clone_repo(clone_dir=archive_dir, ref=ref)
+        clone_repo(
+            clone_dir=archive_dir,
+            clone_address="git@github.com:nsidc/seaice_ecdr.git",
+            ref=seaice_ecdr_ref,
+            project_name="seaice_ecdr",
+        )
+        clone_repo(
+            clone_dir=archive_dir,
+            clone_address="git@github.com:nsidc/pm_icecon.git",
+            ref=pm_icecon_ref,
+            project_name="pm_icecon",
+        )
+        clone_repo(
+            clone_dir=archive_dir,
+            clone_address="git@github.com:nsidc/pm_tb_data.git",
+            ref=pm_tb_data_ref,
+            project_name="pm_tb_data",
+        )
 
         # Copy the ancillary data over
         shutil.copytree(CDR_ANCILLARY_DIR, Path(archive_dir) / CDR_ANCILLARY_DIR.name)
@@ -90,8 +115,27 @@ def make_archive_for_noaa(*, output_dir: Path, ref: str = "main"):
     type=str,
     help="Ref of this code (`seaice_ecdr`) to include in the archive.",
 )
-def cli(output_dir: Path, seaice_ecdr_ref: str):
-    make_archive_for_noaa(output_dir=output_dir, ref=seaice_ecdr_ref)
+@click.option(
+    "--pm-icecon-ref",
+    required=True,
+    type=str,
+    help="Ref of `pm_icecon` to include in the archive.",
+)
+@click.option(
+    "--pm-tb-data-ref",
+    required=True,
+    type=str,
+    help="Ref of `pm_tb_data` to include in the archive.",
+)
+def cli(
+    output_dir: Path, seaice_ecdr_ref: str, pm_icecon_ref: str, pm_tb_data_ref: str
+):
+    make_archive_for_noaa(
+        output_dir=output_dir,
+        seaice_ecdr_ref=seaice_ecdr_ref,
+        pm_icecon_ref=pm_icecon_ref,
+        pm_tb_data_ref=pm_tb_data_ref,
+    )
 
 
 if __name__ == "__main__":
