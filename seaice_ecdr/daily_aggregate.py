add0b79663ab9eb7e236e557a05652b3c0df2a64
Scott <scotts@nsidc.org>
2024-02-02 14:46:13 -0700

    cause daily aggregate for 1978 to begin with 10/25/1978

--- seaice_ecdr/daily_aggregate.py
+++ seaice_ecdr/daily_aggregate.py
@@ -30,7 +30,12 @@ def _get_daily_complete_filepaths_for_year(
     resolution: ECDR_SUPPORTED_RESOLUTIONS,
 ) -> list[Path]:
     data_list = []
-    for period in pd.period_range(start=dt.date(year, 1, 1), end=dt.date(year, 12, 31)):
+    if year == 1978:
+        start_date = dt.date(1978, 10, 25)
+    else:
+        start_date = dt.date(year, 1, 1)
+    # for period in pd.period_range(start=dt.date(year, 1, 1), end=dt.date(year, 12, 31)):
+    for period in pd.period_range(start=start_date, end=dt.date(year, 12, 31)):
         expected_fp = get_ecdr_filepath(
             date=period.to_timestamp().date(),
             hemisphere=hemisphere,
