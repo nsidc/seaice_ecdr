9ba9c37f0edfa55ed928476f9974355079c30c93
Trey Stafford <trey.stafford@colorado.edu>
2024-02-13 14:27:14 -0700

    Add integration test covering case where 0001 channel is missing

--- seaice_ecdr/tests/integration/test_tb_data.py
+++ seaice_ecdr/tests/integration/test_tb_data.py
@@ -1,5 +1,7 @@
+import datetime as dt
+
 import numpy as np
-from pm_tb_data._types import NORTH
+from pm_tb_data._types import NORTH, SOUTH
 
 from seaice_ecdr.platforms import get_platform_start_dates
 from seaice_ecdr.tb_data import get_ecdr_tb_data
@@ -16,3 +18,20 @@ def test_get_ecdr_tb_data():
         assert not np.all(np.isnan(ecdr_tb_data.tbs.v22))
         assert not np.all(np.isnan(ecdr_tb_data.tbs.v37))
         assert not np.all(np.isnan(ecdr_tb_data.tbs.h37))
+
+
+def test_get_ecdr_tb_data_missing_channel():
+    """Test that missing channels from the source get mapped to an all-null grid.
+
+    We know this happens at least once: on 10/10/1995 SH for 22v (from F13).
+    """
+    ecdr_tb_data = get_ecdr_tb_data(date=dt.date(1995, 10, 10), hemisphere=SOUTH)
+
+    # v22 is known to be missing for this day and hemisphere.
+    assert np.all(np.isnan(ecdr_tb_data.tbs.v22))
+
+    # The other channels should continue to exist.
+    assert not np.all(np.isnan(ecdr_tb_data.tbs.v19))
+    assert not np.all(np.isnan(ecdr_tb_data.tbs.h19))
+    assert not np.all(np.isnan(ecdr_tb_data.tbs.v37))
+    assert not np.all(np.isnan(ecdr_tb_data.tbs.h37))
