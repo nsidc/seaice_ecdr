1d5fef47494c2b10f4bdc1e4d1206047239d603d
Trey Stafford <trey.stafford@colorado.edu>
2024-02-21 09:23:47 -0700

    Use standard logger config method to setup logging

--- seaice_ecdr/__init__.py
+++ seaice_ecdr/__init__.py
@@ -3,12 +3,11 @@ import sys
 
 from loguru import logger
 
-# Set the default minimum log notification to Warning
-try:
-    logger.remove(0)  # Removes previous logger info
-except ValueError:
-    logger.debug(f"Started logging in {__name__}")
-
 env = os.environ.get("ENVIRONMENT")
 _default_log_level = "DEBUG" if env == "dev" else "INFO"
-logger.add(sys.stdout, level=_default_log_level)
+
+logger.configure(
+    handlers=[
+        dict(sink=sys.stdout, level=_default_log_level),
+    ]
+)
