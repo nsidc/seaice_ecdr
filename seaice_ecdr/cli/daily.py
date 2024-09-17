f4a87b71363c52ae9557726094b25f3a4fb2d390
Trey Stafford <trey.stafford@colorado.edu>
2024-09-17 09:01:40 -0600

    Update TODOs and improve visibility on hard-coded AMSR2 startdate

--- seaice_ecdr/cli/daily.py
+++ seaice_ecdr/cli/daily.py
@@ -31,9 +31,13 @@ def make_25km_ecdr(
     hemisphere: Hemisphere,
     base_output_dir: Path,
 ):
+    # TODO: consider extracting these to CLI options that default to these values.
     RESOLUTION: Final = "25"
     LAND_SPILLOVER_ALG: Final = "BT_NT"
     ANCILLARY_SOURCE: Final = "CDRv4"
+    # TODO: the amsr2 start date should ideally be read from the platform start
+    # date config.
+    AM2_START_DATE = dt.date(2013, 1, 1)
     # Use the default platform dates, which excldues AMSR2
     _run_cmd(
         f"{CLI_EXE_PATH} multiprocess-intermediate-daily"
@@ -47,9 +51,7 @@ def make_25km_ecdr(
 
     # If the given start & end date intersect with the AMSR2 period, run that
     # separately:
-    # TODO: the amsr2 start date should be read from the platform start date config.
-    am2_start_date = dt.date(2013, 1, 1)
-    if start_date >= am2_start_date:
+    if start_date >= AM2_START_DATE:
         _run_cmd(
             f"export PLATFORM_START_DATES_CONFIG_FILEPATH={PROTOTYPE_PLATFORM_START_DATES_CONFIG_FILEPATH} &&"
             f" {CLI_EXE_PATH} multiprocess-intermediate-daily"
