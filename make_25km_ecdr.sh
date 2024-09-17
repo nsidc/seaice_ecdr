1c6d87c635219207e5ef4cdd109ad3820e6032f3
Trey Stafford <trey.stafford@colorado.edu>
2024-09-17 08:59:09 -0600

    Update `make_25km_ecdr.sh` script

--- make_25km_ecdr.sh
+++ make_25km_ecdr.sh
@@ -9,25 +9,4 @@ START_DATE="2022-03-01"
 END_DATE="2022-04-01"
 HEMISPHERE="north"
 
-# Use the default platform start dates, which excludes AMSR2
-./scripts/cli.sh multiprocess-daily --start-date $START_DATE --end-date $END_DATE --hemisphere $HEMISPHERE --base-output-dir $BASE_OUTPUT_DIR --land-spillover-alg BT_NT --resolution 25 --ancillary-source CDRv4
-
-# Force the use of AMSR2
-PLATFORM_START_DATES_CONFIG_FILEPATH=seaice_ecdr/config/prototype_platform_start_dates.yml ./scripts/cli.sh multiprocess-daily --start-date $START_DATE --end-date $END_DATE --hemisphere $HEMISPHERE --base-output-dir $BASE_OUTPUT_DIR --land-spillover-alg BT_NT --resolution 25 --ancillary-source CDRv4
-
-# Stage files for publication
-./scripts/cli.sh stage-for-publication --date $START_DATE --end-date $END_DATE --hemisphere $HEMISPHERE --base-output-dir $BASE_OUTPUT_DIR --resolution 25
-
-
-# Combine where there's overlap between the two, and publish to an output dir
-# that's ready for publication.
-
-# TODO:
-# * Change how checksum files are generated. They won't be "complete" anymore,
-#   they'll be in "ready_for_publication" or something similar (could change
-#   current `complete` to a subdir of `intermediate`). Should probably start by changing the output dir for complete daily to an intermediate dir.
-# * Change when/how aggregrate files are generated. They should include the
-#   prototype fields.
-# * validation/qa script needs to be updated to support new outputs. How do we
-#   validate prototype fields?
-# * 
+./scripts/cli.sh daily --start-date $START_DATE --end-date $END_DATE --hemisphere $HEMISPHERE --base-output-dir $BASE_OUTPUT_DIR
