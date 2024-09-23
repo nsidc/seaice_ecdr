ee18544d4610c4052336c49891e9fc070217961a
Trey Stafford <trey.stafford@colorado.edu>
2024-09-23 17:12:50 -0600

    Cleanup now-unused constant

--- seaice_ecdr/constants.py
+++ seaice_ecdr/constants.py
@@ -65,10 +65,6 @@ DEFAULT_BASE_OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 LOGS_DIR = NSIDC_NFS_SHARE_DIR / f"{ECDR_PRODUCT_VERSION}_logs" / _env_subdir
 LOGS_DIR.mkdir(parents=True, exist_ok=True)
 
-# Location of LANCE AMSR2 NRT data files:
-# TODO: nest the subdir under an `ecdr_inputs` or similar?
-LANCE_NRT_DATA_DIR = NSIDC_NFS_SHARE_DIR / "lance_amsr2_nrt_data"
-
 # Location of surface mask & geo-information files.
 CDR_ANCILLARY_DIR = NSIDC_NFS_SHARE_DIR / f"{ECDR_PRODUCT_VERSION}_ancillary"
 CDRv4_ANCILLARY_DIR = NSIDC_NFS_SHARE_DIR / "cdrv4_equiv_ancillary"
