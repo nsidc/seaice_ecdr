f15467fdde5bc12a5154656bb9559a75076bea5e
Trey Stafford <trey.stafford@colorado.edu>
2025-07-07 12:43:11 -0600

    Add support for producing monthly NRT from 0802
    Retains the code to produce a prototype group, but makes it optional. This still
    feels a little hacky, and might be nice to just remove the code to support the
    "prototype" group altogether, but it may yet come in handy w/ e.g., AMSR3

--- seaice_ecdr/cli/monthly_nrt.py
+++ seaice_ecdr/cli/monthly_nrt.py
@@ -13,7 +13,6 @@ from seaice_ecdr.constants import (
 )
 from seaice_ecdr.platforms.config import (
     NRT_AM2_PLATFORM_START_DATES_CONFIG_FILEPATH,
-    NRT_F17_PLATFORM_START_DATES_CONFIG_FILEPATH,
 )
 
 
@@ -24,19 +23,13 @@ def make_monthly_25km_ecdr(
     end_month: int | None,
     hemisphere: Hemisphere,
     base_output_dir: Path,
-    nrt_platform_id: Literal["F17", "am2"],
 ):
     if end_year is None:
         end_year = year
     if end_month is None:
         end_month = month
 
-    if nrt_platform_id == "F17":
-        nrt_platform_start_dates_filepath = NRT_F17_PLATFORM_START_DATES_CONFIG_FILEPATH
-    elif nrt_platform_id == "am2":
-        nrt_platform_start_dates_filepath = NRT_AM2_PLATFORM_START_DATES_CONFIG_FILEPATH
-    else:
-        raise RuntimeError(f"NRT processing is not defined for {nrt_platform_id}")
+    nrt_platform_start_dates_filepath = NRT_AM2_PLATFORM_START_DATES_CONFIG_FILEPATH
     run_cmd(
         f"export PLATFORM_START_DATES_CONFIG_FILEPATH={nrt_platform_start_dates_filepath} &&"
         f" {CLI_EXE_PATH} intermediate-monthly"
@@ -117,15 +110,6 @@ def make_monthly_25km_ecdr(
     ),
     show_default=True,
 )
-@click.option(
-    "--nrt-platform-id",
-    # TODO: eventually we want to support AM2 as a separate monthly file. This
-    # does not currently work: the code instead places the AMSR2 monthly data
-    # into a `am2_prototype` group like the non-nrt files, and that's not what
-    # we want because the lag on data for F17 is different from AM2.
-    type=click.Choice(["F17"]),
-    default="F17",
-)
 def cli(
     *,
     year: int,
@@ -134,7 +118,6 @@ def cli(
     end_month: int | None,
     hemisphere: Hemisphere | Literal["both"],
     base_output_dir: Path,
-    nrt_platform_id: Literal["F17"],
 ) -> None:
     base_output_dir = base_output_dir / "CDR"
     base_output_dir.mkdir(exist_ok=True)
@@ -152,7 +135,6 @@ def cli(
             end_month=end_month,
             hemisphere=hemisphere,
             base_output_dir=base_output_dir,
-            nrt_platform_id=nrt_platform_id,
         )
 
 
